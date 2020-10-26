a8612bd7421bb25d9a2bc770edec1e75f6710c00
K <651395250@qq.com>
2020-10-26 21:23:10 +0800

    update git test

--- com/kkb/GitTest/GitDemo.java
+++ com/kkb/GitTest/GitDemo.java
@@ -3,5 +3,6 @@ package com.kkb.GitTest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git！");
+        System.out.println("Hello Git2！");
     }
 }
